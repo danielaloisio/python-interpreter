85f466a3e2bceed36363bf420e99470d1a164be5
Daniel Aloísio <danielaloisio@Daniels-MacBook-Pro.local>
2026-09-12 23:06:14 -0300

    Add function support

---
+++ src/interpreter/Node/ReturnNode.h
@@ -0,0 +1,33 @@
+/*
+Copyright (C) 2025 M3t4l
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see
+<https://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef RETURNNODE_H
+#define RETURNNODE_H
+
+
+class ReturnNode : public ASTNode {
+public:
+    std::shared_ptr<ASTNode> value;
+
+    ReturnNode(std::shared_ptr<ASTNode> v) : value(v) {
+    }
+};
+
+
+#endif //RETURNNODE_H
